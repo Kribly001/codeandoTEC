0cdd986850b6cc7386cff26170a67228ece1044d
Maxi Centeno <contactmaxi531@gmail.com>
2022-11-18 21:15:18 -0300

    SEO

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,12 @@
 const typed= new Typed('.typed', {
     strings:['&lt;CodeandoTEC/&gt;'],
-    	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
-	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: false, // Repetir el array de strings
-	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-	showCursor: true, // Mostrar cursor palpitanto
-	cursorChar: '|', // Caracter para el cursor
-	contentType: 'html', // 'html' o 'null' para texto sin formato
+	typeSpeed: 75, 
+	startDelay: 300,  
+	backSpeed: 75, 
+	backDelay: 1500, 
+	loop: false, 
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '|', 
+	contentType: 'html', 
 })
